4c2b6271fe7bc4abb9e8154bc9db0472a617609e
Erik Zemčík <zemcik.erik@gmail.com>
2020-11-23 19:31:44 +0100

    Finished fourth exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { ItemsComponent } from './items/items.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    ItemsComponent
+    ItemsComponent,
+    HeroDetailComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule,
